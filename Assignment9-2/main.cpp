2e440a9367b95b83fbb4da99b0334457da771253
penguin <penguin586970@gmail.com>
2025-07-18 00:01:38 -0400

    Added character function with checks that throw exceptions

--- Assignment9-2/main.cpp
+++ Assignment9-2/main.cpp
@@ -2,9 +2,27 @@
 // CIS 1202 800
 // July 17, 2025
 
+#include <iostream>
 using namespace std;
+
+char character(char, int);
 
 int main(int argc, const char * argv[]) {
     
     return 0;
+}
+
+
+char character(char start, int offset){
+    if (!isalpha(start)){
+        throw "Error: Invalid Character";
+    }
+    
+    char result = start + offset;
+    
+    if (!isalpha(result)){
+        throw "Error: Invalid Range";
+    }
+    
+    return result;
 }
