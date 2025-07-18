68c86533609dd53db3cbd36ec910ef08c4650ff5
penguin <penguin586970@gmail.com>
2025-07-17 23:46:09 -0400

    Initial Commit

---
+++ Assignment9-2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment9-2
+//
+//  Created by Ethan Yao on 7/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
